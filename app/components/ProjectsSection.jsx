d20bab2963bcff111be5abf5ad4d7176e894b1f3
Saundarya021 <129167751+Saundarya021@users.noreply.github.com>
2024-06-26 18:40:47 +0530

    updated

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -73,6 +73,7 @@ const ProjectsSection = () => {
 
   return (
    <>
+   <section id="projects">
     <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
       My Projects
     </h2>
@@ -88,6 +89,7 @@ const ProjectsSection = () => {
           />
         ))}
     </div>
+    </section>
    </>
   );
 };
